3a88a7a1f42eeb1db67293093eeec61298c68a0d
zeraati <zeraati@csis.local>
2026-02-15 08:05:19 +0330

    *

---
+++ Common/Config/Auth/1.Jwt.cs
@@ -0,0 +1,29 @@
+﻿using FastEndpoints.Security;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Common;
+public static partial class ServiceConfig
+{
+    public static IServiceCollection AddAuthConfig(this IServiceCollection services, string signingKey, string issuer, string audience)
+    {
+        services
+            .Configure<JwtCreationOptions>(x =>
+            {
+                x.SigningKey = signingKey;
+                x.ExpireAt = DateTime.UtcNow.Add(AuthConfig.TokenLifetime);
+                x.Issuer = issuer;
+                x.Audience = audience;
+            })
+            .Configure<JwtSigningOptions>(x => { x.SigningKey = signingKey; })
+
+            .AddAuthenticationJwtBearer(x => { },
+                x =>
+                {
+                    x.TokenValidationParameters.ValidIssuer = issuer;
+                    x.TokenValidationParameters.ValidAudience = audience;
+                })
+            .AddAuthorization();
+
+        return services;
+    }
+}
