fe3e8596180b373fc067a59261e250787adcd205
zeraati <zeraati@csis.local>
2026-02-21 07:45:18 +0330

    *

---
+++ FastEndpoint/Feature/02.User/2.Create/Request.cs
@@ -0,0 +1,2 @@
+﻿namespace FastEndpoint.Feature.Endpoint;
+public record UserCreateRequest(string? FirstName,string? LastName,string UserName,string Password);
---
+++ FastEndpoint/Feature/02.User/3.Get/Response.cs
@@ -0,0 +1,10 @@
+﻿using FastEndPoint.Feature.Domain;
+
+namespace FastEndpoint.Feature.Endpoint;
+public class UserGetResponse(User user)
+{
+    public int Id { get; } = user.Id;
+    public string? FirstName { get;} = user.FirstName;
+    public string? LastName { get; } = user.LastName;
+    public string UserName { get;} = user.UserName;
+}
