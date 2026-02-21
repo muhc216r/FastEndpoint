b814c67b5075c351010d1730d8d50c5369b9e430
zeraati <zeraati@csis.local>
2026-02-21 07:59:07 +0330

    *

--- FastEndpoint/Feature/02.User/3.Get/Endpoint.cs
+++ FastEndpoint/Feature/02.User/3.Get/Endpoint.cs
@@ -12,7 +12,7 @@ public class UserGet(AppDbContext db) : EndpointWithoutRequest<UserGetResponse>
 
     public override async Task HandleAsync(CancellationToken cancellation)
     {
-        int id = Route<int>("id");
+        var id = Route<int>("id");
         var user = await db.Set<User>().SingleAsync(x=>x.Id==id,cancellation);
         Response = new UserGetResponse(user!);
     }
